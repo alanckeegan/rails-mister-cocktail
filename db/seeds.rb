9c5d99e7f456b7f3381435ddb54824c4449a4bb4
Alan Keegan <alan.c.keegan@gmail.com>
2018-11-23 12:22:53 +0100

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Cocktail.new(name: "Gin Martini").save
+Cocktail.new(name: "Negroni").save
+Cocktail.new(name: "Rob Roy").save
+Cocktail.new(name: "Old Fashioned").save
+
+Ingredient.new(name: "Orange").save
+Ingredient.new(name: "Dry Vermouth").save
+Ingredient.new(name: "Sweet Vermouth").save
+Ingredient.new(name: "Gin").save
+Ingredient.new(name: "Campari").save
+Ingredient.new(name: "Whiskey").save
+Ingredient.new(name: "Sugar").save
+Ingredient.new(name: "Bitters").save
+Ingredient.new(name: "Maraschino Cherry").save
